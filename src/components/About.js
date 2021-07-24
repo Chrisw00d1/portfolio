7939e766e4af06a62d0d109897a7887f46bef5b7
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-07-24 21:22:45 +0100

    changed about me and added images to cloudinary

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,15 @@ export default function About() {
 
   return (
     <div className='about'>
-      <img src='https://i.stack.imgur.com/l60Hf.png' alt='profile pic'/>
+      <img src='https://res.cloudinary.com/dqdugu41a/image/upload/v1627140093/Portfolio/LinkedIn_Pic_h7zoee.jpg' alt='profile pic' />
       <div className='aboutText'>
         <h2>Chris Wood</h2>
         <p>I graduated in 2020 with BEng (Hons) first class degree of Mechanical Engineering from the University of Swansea.
           Throughout the course I was exposed to programming which ranged from analysing data to getting a robot arm to mimic my arm movement.
           The designing and building process in coding sparked the same interest with the design process I got from my engineering course. <br></br><br></br>
-          Once I finished University I decided to pursue this new interest I had in coding as I did not need to be in a dedicated workshop to be able to work on projects. I joined a remote software engineering immersive course with General Assembly which covers the fundamentals of Web Development with the aim of being able to apply the skills I learnt from the course into a Software Engineering role.</p>
+          Once I finished University I decided to pursue this new interest I had in coding as I did not need to be in a dedicated workshop to be able to work on projects. I joined a remote software engineering immersive course with General Assembly which covers the fundamentals of Web Development with the aim of being able to apply the skills I learnt from the course into a Software Engineering role. <br></br><br></br>
+          I am currently working on a project with a friend which involves connecting a raspberry pi to a website so that we will be able to control it from our phones. Outside of coding I like to go bouldering when I can with friends or to try and beat escape rooms. I’ve also grown up playing the drums which led to me playing in a bunch of charity concerts at school and then to joining a band at uni. We published and released a song while a uni called honey gold which you can give a listen to below. </p>
+          <iframe width="100%" height="300" src="https://www.youtube.com/embed/VznDBjhJPRc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   )
--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -1,21 +1,8 @@
-import battleships1 from '../images/battleships1.png'
-import battleships2 from '../images/battleships2.png'
-import battleships3 from '../images/battleships3.png'
-import superhero1 from '../images/superhero1.png'
-import superhero2 from '../images/superhero2.png'
-import superhero3 from '../images/superhero3.png'
-import r8mypl81 from '../images/r8mypl81.png'
-import r8mypl82 from '../images/r8mypl82.png'
-import r8mypl83 from '../images/r8mypl83.png'
-import noteitdown1 from '../images/noteitdown1.png'
-import noteitdown2 from '../images/noteitdown2.png'
-import noteitdown3 from '../images/noteitdown3.png'
-
 const projectData = [
   {
     projectId: 1,
     projectName: 'Battleships',
-    images: [battleships1, battleships2, battleships3],
+    images: ['https://res.cloudinary.com/dqdugu41a/image/upload/v1627156996/Portfolio/battleships1_rrqwab.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157004/Portfolio/battleships2_mutloh.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157008/Portfolio/battleships3_dxlw5u.png'],
     skills: ['devicon-html5-plain', 'devicon-css3-plain', 'devicon-javascript-plain', 'devicon-git-plain', 'devicon-github-original'],
     links: ['https://github.com/Chrisw00d1/battleships', 'https://chrisw00d1.github.io/battleships/'],
     description: 'My take on the board game Battleships. You battle against the computer to see who can sink their oppenents ships first! This was created using HTML, CSS and JavaScript.\nTime-frame: 1 week'
@@ -24,7 +11,7 @@ const projectData = [
   {
     projectId: 2,
     projectName: 'Superhero TopTrumps',
-    images: [superhero1, superhero2, superhero3],
+    images: ['https://res.cloudinary.com/dqdugu41a/image/upload/v1627157058/Portfolio/superhero1_d999nb.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157062/Portfolio/superhero2_tvjmau.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157068/Portfolio/superhero3_fibvye.png'],
     skills: ['devicon-html5-plain', 'devicon-css3-plain', 'devicon-javascript-plain', 'devicon-react-original', 'devicon-sass-original', 'devicon-git-plain', 'devicon-github-original'],
     links: ['https://github.com/Chrisw00d1/Project-2', 'https://superherocards.netlify.app/'],
     description: 'A chance to see all your favourite superheros on cards and then being able to use them to play against each other in our version of top trumps. We used React to create the site and an API to get the data.\nTime-frame: 48 hours'
@@ -34,7 +21,7 @@ const projectData = [
 
     projectId: 3,
     projectName: 'R8 my Pl8 M8',
-    images: [r8mypl81, r8mypl82, r8mypl83],
+    images: ['https://res.cloudinary.com/dqdugu41a/image/upload/v1627157144/Portfolio/r8mypl81_m9igug.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157152/Portfolio/r8mypl82_noe7wt.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157161/Portfolio/r8mypl83_hirp7m.png'],
     skills: ['devicon-html5-plain', 'devicon-css3-plain', 'devicon-javascript-plain', 'devicon-react-original', 'devicon-nodejs-plain', 'devicon-mongodb-plain', 'devicon-express-original', 'devicon-sass-original', 'devicon-bulma-plain', 'devicon-git-plain', 'devicon-github-original', 'devicon-heroku-original'],
     links: ['https://github.com/Chrisw00d1/project-3-client', 'https://r8-my-pl8.netlify.app/'],
     description: 'We created a place for you to share your best plates with your mates for them to rate and comment on. On top of this you can see how many plates you can catch before dropping one. The front end was created with React and the back end was set up with express and mongodb.\nTime-frame: 1 week'
@@ -44,7 +31,7 @@ const projectData = [
 
     projectId: 4,
     projectName: 'note-it-down',
-    images: [noteitdown1, noteitdown2, noteitdown3],
+    images: ['https://res.cloudinary.com/dqdugu41a/image/upload/v1627157180/Portfolio/noteitdown1_cz9gg6.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157186/Portfolio/noteitdown2_zjxsmp.png', 'https://res.cloudinary.com/dqdugu41a/image/upload/v1627157674/Portfolio/noteitdown3_emaodd.png'],
     skills: ['devicon-html5-plain', 'devicon-css3-plain', 'devicon-javascript-plain', 'devicon-react-original', 'devicon-python-plain', 'devicon-django-plain', 'devicon-postgresql-plain', 'devicon-sass-original', 'devicon-git-plain', 'devicon-github-original', 'devicon-heroku-original', 'devicon-trello-plain'],
     links: ['https://github.com/Chrisw00d1/project-4-client', 'https://note-it-down-net.netlify.app/'],
     description: 'A chance for you to note down that tune in your head and share it with others. We used React and Tone.js for the front end and python with django for the back end.\nTime-frame: 1 week'
